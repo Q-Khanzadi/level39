be42db497ff4ad1f162f7cc62e5b13ee0ebc9b19
sophiegorno <sg5115@ic.ac.uk>
2017-03-03 14:35:04 +0000

    Profile markers
    Added a simple profile marker (name and age) for each city and made code
    slightly shorter by putting lat/long coords straight in without
    variables

--- World_map.js
+++ World_map.js
@@ -1,11 +1,4 @@
 function initMap() {
-  var London = {lat: 51.5074, lng: -0.1278};
-  var Paris = {lat: 48.8566, lng: 2.3522};
-  var NewYork = {lat: 40.7128, lng: -74.0059};
-  var Sydney = {lat: -33.8688, lng: 151.2093};
-  var Tokyo = {lat: 35.6895, lng: 139.6917};
-  var Rio = {lat: -22.9068, lng: -43.1729};
-  var LosAng = {lat: 34.0522, lng: -118.2437};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 1,
     minZoom: 1,
@@ -13,7 +6,7 @@ function initMap() {
   });
 
   var marker_London = new google.maps.Marker({
-    position: London,
+    position: {lat: 51.5074, lng: -0.1278},
     map: map,
     title: "London",
   });
@@ -26,34 +19,81 @@ function initMap() {
   });
 
   var marker_Paris = new google.maps.Marker({
-    position: Paris,
+    position: {lat: 48.8566, lng: 2.3522},
     map: map,
     title: "Paris",
   });
+  var contentString_Paris = "Jean - 30";
+  var infowindow_Paris = new google.maps.InfoWindow({
+    content: contentString_Paris
+  });
+  marker_Paris.addListener('click', function() {
+    infowindow_Paris.open(map, marker_Paris);
+  });
+
   var marker_NewYork = new google.maps.Marker({
-    position: NewYork,
+    position: {lat: 40.7128, lng: -74.0059},
     map: map,
     title: "New York",
   });
+  var contentString_NewYork = "Cindy - 24";
+  var infowindow_NewYork = new google.maps.InfoWindow({
+    content: contentString_NewYork
+  });
+  marker_NewYork.addListener('click', function() {
+    infowindow_NewYork.open(map, marker_NewYork);
+  });
+
   var marker_Sydney = new google.maps.Marker({
-    position: Sydney,
+    position: {lat: -33.8688, lng: 151.2093},
     map: map,
     title: "Sydney",
   });
+  var contentString_Sydney = "Brian - 36";
+  var infowindow_Sydney = new google.maps.InfoWindow({
+    content: contentString_Sydney
+  });
+  marker_Sydney.addListener('click', function() {
+    infowindow_Sydney.open(map, marker_Sydney);
+  });
+
   var marker_Tokyo = new google.maps.Marker({
-    position: Tokyo,
+    position: {lat: 35.6895, lng: 139.6917},
     map: map,
     title: "Tokyo",
   });
+  var contentString_Tokyo = "Isobel - 24";
+  var infowindow_Tokyo = new google.maps.InfoWindow({
+    content: contentString_Tokyo
+  });
+  marker_Tokyo.addListener('click', function() {
+    infowindow_Tokyo.open(map, marker_Tokyo);
+  });
+
   var marker_LosAng = new google.maps.Marker({
-    position: LosAng,
+    position: {lat: 34.0522, lng: -118.2437},
     map: map,
     title: "Los Angeles",
   });
+  var contentString_LosAng = "Anna - 25";
+  var infowindow_LosAng = new google.maps.InfoWindow({
+    content: contentString_LosAng
+  });
+  marker_LosAng.addListener('click', function() {
+    infowindow_LosAng.open(map, marker_LosAng);
+  });
+
   var marker_Rio = new google.maps.Marker({
-    position: Rio,
+    position: {lat: -22.9068, lng: -43.1729},
     map: map,
     title: "Rio de Janeiro",
+  });
+  var contentString_Rio = "Paolo - 27";
+  var infowindow_Rio = new google.maps.InfoWindow({
+    content: contentString_Rio
+  });
+  marker_Rio.addListener('click', function() {
+    infowindow_Rio.open(map, marker_Rio);
   });
 
 };
